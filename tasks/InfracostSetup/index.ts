eb9c5287b5418627a91bda0eef0d8ae597ad16b6
tim775 <52185+tim775@users.noreply.github.com>
2023-02-22 16:10:19 -0500

    fix: revert whitespace change

--- tasks/InfracostSetup/index.ts
+++ tasks/InfracostSetup/index.ts
@@ -260,7 +260,7 @@ function exportEnvVars() {
   if (repoUrl) {
     taskLib.setVariable('INFRACOST_VCS_REPOSITORY_URL', repoUrl);
   }
-
+  
   const logLevel = taskLib.getVariable('System.Debug') === 'true' ? 'debug' : 'info';
   taskLib.setVariable('INFRACOST_LOG_LEVEL', logLevel)
 }
