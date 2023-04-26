8f5cc72feb89c45f80c17e891f9953e8714fa8a9
Alistair Scott <ali.scott@gmail.com>
2023-04-26 18:50:08 +0100

    fix: fix Terraform validation error

--- examples/plan-json/terraform-cloud-enterprise/code/main.tf
+++ examples/plan-json/terraform-cloud-enterprise/code/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web_app" {
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world"
-  role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
+  role          = "arn:aws:lambda:us-east-1:111111111111:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
   filename      = "function.zip"
